7133dcbc76a48193f4ddaeb53f3a3aa9e193f740
vatskishan03 <kishanvats2003@gmail.com>
2026-07-27 19:23:20 +0000

    Split case-13 terminal 299 kernel proof

---
+++ MQGN6Audit/UniqueDagStructuralKernelCase6_13_Terminal299AllowedV4.lean
@@ -0,0 +1,12 @@
+import MQGN6Audit.UniqueDagStructuralKernelCase6_13_Base
+
+namespace MQGN6Audit
+
+set_option maxRecDepth 1000000 in
+set_option maxHeartbeats 10000000 in
+theorem case13Terminal299AllowedV4Possible6 :
+    terminalForcesCodeStructural6 (uniqueDagCase6 13) 299 4 fun code =>
+      codeAllowsMatchingAtFast6 code 4 (decodeDagColoringFast6 261) 14 := by
+  decide +kernel
+
+end MQGN6Audit
