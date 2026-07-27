cf612e77ab66ed0f28081019047587a8f072ce1b
vatskishan03 <kishanvats2003@gmail.com>
2026-07-27 21:50:47 +0530

    Check terminal 245 allowedness at vertex 2

---
+++ MQGN6Audit/UniqueDagStructuralKernelCase6_13_Terminal245AllowedV2.lean
@@ -0,0 +1,12 @@
+import MQGN6Audit.UniqueDagStructuralKernelCase6_13_Base
+
+namespace MQGN6Audit
+
+set_option maxRecDepth 1000000 in
+set_option maxHeartbeats 10000000 in
+theorem case13Terminal245AllowedV2Possible6 :
+    terminalForcesCodeStructural6 (uniqueDagCase6 13) 245 2 fun code =>
+      codeAllowsMatchingAtFast6 code 2 (decodeDagColoringFast6 1258) 12 := by
+  decide +kernel
+
+end MQGN6Audit
