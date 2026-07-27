9073cb7dd07e11cd69ea39bcbf6df66ad2f0382c
vatskishan03 <kishanvats2003@gmail.com>
2026-07-27 22:59:18 +0530

    Check terminal 273 allowed vertex 4

---
+++ MQGN6Audit/UniqueDagStructuralKernelCase6_13_Terminal273AllowedV4.lean
@@ -0,0 +1,12 @@
+import MQGN6Audit.UniqueDagStructuralKernelCase6_13_Base
+
+namespace MQGN6Audit
+
+set_option maxRecDepth 1000000 in
+set_option maxHeartbeats 10000000 in
+theorem case13Terminal273AllowedV4Possible6 :
+    terminalForcesCodeStructural6 (uniqueDagCase6 13) 273 4 fun code =>
+      codeAllowsMatchingAtFast6 code 4 (decodeDagColoringFast6 27) 12 := by
+  decide +kernel
+
+end MQGN6Audit
