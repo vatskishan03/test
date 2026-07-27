3173c60bd49e6dabaae8735ed92f2d014c69aba5
Kishan <kishanvats2003@gmail.com>
2026-07-27 07:53:10 +0530

    Use fast base-six DAG replay in root build

--- MQGN6Audit.lean
+++ MQGN6Audit.lean
@@ -13,4 +13,4 @@ import MQGN6Audit.VertexRelabelScratch
 import MQGN6Audit.VertexRelabel
 import MQGN6Audit.TargetOrbits6
 import MQGN6Audit.FiniteDefinitions6
-import MQGN6Audit.UniqueDagCheck6
+import MQGN6Audit.UniqueDagCheckFast6
