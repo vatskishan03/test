ffd2f64e75188ab1333c4c1e65d39a13893ed8c7
Kishan <kishanvats2003@gmail.com>
2026-07-27 07:15:06 +0530

    Replace exhaustive classifier with compressed DAG replay

--- MQGN6Audit.lean
+++ MQGN6Audit.lean
@@ -12,5 +12,5 @@ import MQGN6Audit.UniqueObstruction
 import MQGN6Audit.VertexRelabelScratch
 import MQGN6Audit.VertexRelabel
 import MQGN6Audit.TargetOrbits6
-import MQGN6Audit.FiniteClassification6
-import MQGN6Audit.TerminalFinite6
+import MQGN6Audit.FiniteDefinitions6
+import MQGN6Audit.UniqueDagCheck6
