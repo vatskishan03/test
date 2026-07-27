5dab7c39c23d8b8078fa151fdde244ca66bb5605
vatskishan03 <kishanvats2003@gmail.com>
2026-07-27 23:42:33 +0000

    Split case-13 terminal 437 kernel proof

---
+++ MQGN6Audit/UniqueDagStructuralKernelCase6_13_Terminal437ForcedBase.lean
@@ -0,0 +1,21 @@
+import MQGN6Audit.UniqueDagStructuralKernelCase6_13_Base
+
+namespace MQGN6Audit
+
+def case13Terminal437ForcedAtPossible6 (k : Fin 3) : Prop :=
+  let e := matchingEdges6 12 k
+  ((decodeDagColoringFast6 119) e.1 =
+      (decodeDagColoringFast6 119) e.2 ∧
+    matchingMate6
+      ((feasibleTargetRep6 13) ((decodeDagColoringFast6 119) e.1))
+      e.1 = e.2) ∨
+    ((terminalForcesCodeStructural6 (uniqueDagCase6 13) 437 e.1
+        fun code =>
+          decodeLocalPlanFast6 code
+            ((decodeDagColoringFast6 119) e.2) = e.2) ∧
+     (terminalForcesCodeStructural6 (uniqueDagCase6 13) 437 e.2
+        fun code =>
+          decodeLocalPlanFast6 code
+            ((decodeDagColoringFast6 119) e.1) = e.1))
+
+end MQGN6Audit
