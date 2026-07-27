e1ab5ae3e9b8d9f9a5a031a3b65fafcb65641b95
vatskishan03 <kishanvats2003@gmail.com>
2026-07-27 23:45:07 +0530

    Check terminal 285 allowed vertex 5

---
+++ MQGN6Audit/UniqueDagStructuralKernelCase6_13_Terminal285AllowedV5.lean
@@ -0,0 +1,12 @@
+import MQGN6Audit.UniqueDagStructuralKernelCase6_13_Base
+
+namespace MQGN6Audit
+
+set_option maxRecDepth 1000000 in
+set_option maxHeartbeats 10000000 in
+theorem case13Terminal285AllowedV5Possible6 :
+    terminalForcesCodeStructural6 (uniqueDagCase6 13) 285 5 fun code =>
+      codeAllowsMatchingAtFast6 code 5 (decodeDagColoringFast6 29) 12 := by
+  decide +kernel
+
+end MQGN6Audit
